e37d7309d5cc7df2d972e9dbf43a5cab0965f9c4
mmcnew <mike@visceralconcepts.com>
2015-11-02 17:27:53 -0800

    Finalizing Work (Hopefully)

--- js/gmaps.js
+++ js/gmaps.js
@@ -5,9 +5,9 @@
         var latLng = new google.maps.LatLng(position[0], position[1]);
      
         var mapOptions = {
-            zoom: 16, // initialize zoom level - the max value is 21
+            zoom: 18, // initialize zoom level - the max value is 21
             streetViewControl: false, // hide the yellow Street View pegman
-            scaleControl: true, // allow users to zoom the Google Map
+            scaleControl: false, // allow users to zoom the Google Map
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             center: latLng
         };
